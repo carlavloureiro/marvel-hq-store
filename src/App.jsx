0459792a9fd686cf096b970cef9720c11076f4e4
Carla Loureiro <116118483+carlavloureiro@users.noreply.github.com>
2025-08-11 01:04:12 +0000

    feat: Adição mensagem secreta do console

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
+  useEffect(() => {
+    console.log("Bem-vindo, herói da NeoApp!");
+    console.log("Antes de ser dev, fui fã de heróis...");
+    console.log("Mas depois de aprender React e Styled Components, me tornei minha própria super-heroína do código! ⚡️");
+    console.log("Este projeto é minha primeira missão, e prometo que as próximas serão ainda maiores!");
+    console.log("Se você quer um dev que nunca desiste, aqui estou! Pronta para derrotar bugs malignos e salvar seu código!");
+  }, []);
+
   return (
     <Router>
-      <GlobalStyle isCartPage={location.pathname === '/cart'}/>
+      <GlobalStyle />
       <AppRoutes />
     </Router>
-  );a
+  );
 }
 
 export default App;
